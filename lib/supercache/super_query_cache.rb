abcc520adf63863dceac08f20a5d67fef26b27ca
ethiraj <ethirajsrinivasan@gmail.com>
2016-09-17 00:00:52 +0530

    Fix cache when rails restart

--- lib/supercache/super_query_cache.rb
+++ lib/supercache/super_query_cache.rb
@@ -1,7 +1,7 @@
 module SuperQueryCache
   def cache_sql(sql, binds)
     if Rails.cache.read(:ar_supercache)
-      Rails.cache.fetch("supercache_#{sql}_#{binds.to_s}".hash) do
+      Rails.cache.fetch(Digest::SHA1.hexdigest("supercache_#{sql}_#{binds.to_s}")) do
         super(sql, binds)
       end
     else
