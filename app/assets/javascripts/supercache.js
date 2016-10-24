3191fca329aa71135c5d0beb9349dae6d818d713
ethiraj <ethirajsrinivasan@gmail.com>
2016-10-24 17:36:19 +0530

    Add Http Spec and replace CDN

---
+++ app/assets/javascripts/supercache.js
@@ -0,0 +1,2 @@
+//= require jquery-2.1.1.min
+//= require materialize.min
--- spec/test_app/config/routes.rb
+++ spec/test_app/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: :index
+  resources :users, only: :index do
+    collection do
+      get :http_request
+    end
+  end
 end
