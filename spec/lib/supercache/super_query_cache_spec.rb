4b65719ed93e86e65992ff6b111da560cfc562ee
ethiraj <ethirajsrinivasan@gmail.com>
2017-04-24 21:34:08 +0800

    remove support for 1.9.3

--- spec/lib/supercache/super_query_cache_spec.rb
+++ spec/lib/supercache/super_query_cache_spec.rb
@@ -1,15 +1,7 @@
 require 'spec_helper'
 
-if RUBY_VERSION < "2.0"
-  describe ActiveRecord::ConnectionAdapters::QueryCache do
-    it 'should overwrite cache_sql with cache_sql_with_superquerycache' do
-     expect(subject.instance_method(:cache_sql)).to eq ActiveRecord::ConnectionAdapters::QueryCache.instance_method(:cache_sql_with_superquerycache)
-    end
-  end
-else
-  describe SuperQueryCache do
-    it 'should include SuperQueryCache in  ActiveRecord::ConnectionAdapters::AbstractAdapter ancestors' do
-     expect(ActiveRecord::ConnectionAdapters::AbstractAdapter.ancestors).to include subject
-    end
+describe SuperQueryCache do
+  it 'should include SuperQueryCache in  ActiveRecord::ConnectionAdapters::AbstractAdapter ancestors' do
+   expect(ActiveRecord::ConnectionAdapters::AbstractAdapter.ancestors).to include subject
   end
 end
