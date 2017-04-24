6faed139fcab9f7a674d92e98edab415ecf562e8
ethiraj <ethirajsrinivasan@gmail.com>
2017-04-25 01:56:19 +0800

    Remove support for Ruby 1.9.3 (#13)
    * fix security issue
    
    * remove support for 1.9.3

--- lib/supercache/super_query_cache.rb
+++ lib/supercache/super_query_cache.rb
@@ -1,37 +1,14 @@
-if RUBY_VERSION < "2.0"
-  module ActiveRecord
-    module ConnectionAdapters # :nodoc:
-      module QueryCache
-        def cache_sql_with_superquerycache(*args, &block)
-          if Rails.cache.read(:ar_supercache)
-            sub_key = args[1].collect{|a| "#{a.try(:name)} #{a.try(:value)}"}
-            Rails.cache.fetch(Digest::SHA1.hexdigest("supercache_#{args[0]}_#{sub_key}")) do
-              request_without_superquerycache(*args, &block)
-            end
-          else
-            request_without_superquerycache(*args, &block)
-          end
-        end
-
-        alias_method :request_without_superquerycache, :cache_sql
-        alias_method :cache_sql, :cache_sql_with_superquerycache
-
-      end
-    end
-  end
-else
-  module SuperQueryCache
-    def cache_sql(*args, &block)
-      if Rails.cache.read(:ar_supercache)
-        sub_key = args[1].collect{|a| "#{a.try(:name)} #{a.try(:value)}"}
-        Rails.cache.fetch(Digest::SHA1.hexdigest("supercache_#{args[0]}_#{sub_key}")) do
-          super(*args, &block)
-        end
-      else
+module SuperQueryCache
+  def cache_sql(*args, &block)
+    if Rails.cache.read(:ar_supercache)
+      sub_key = args[1].collect{|a| "#{a.try(:name)} #{a.try(:value)}"}
+      Rails.cache.fetch(Digest::SHA1.hexdigest("supercache_#{args[0]}_#{sub_key}")) do
         super(*args, &block)
       end
+    else
+      super(*args, &block)
     end
   end
-
-  ActiveRecord::ConnectionAdapters::AbstractAdapter.prepend(SuperQueryCache)
 end
+
+ActiveRecord::ConnectionAdapters::AbstractAdapter.prepend(SuperQueryCache)
