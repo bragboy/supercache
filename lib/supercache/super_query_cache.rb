ebfb6eb9f99761507d1499143a4740965b24c3b5
Bragadeesh <bragboy@gmail.com>
2017-12-14 14:44:06 +0530

    Remove pry statement

--- lib/supercache/super_query_cache.rb
+++ lib/supercache/super_query_cache.rb
@@ -14,7 +14,6 @@ end
 
 module SuperQueryCache
   def cache_sql(*args, &block)
-    binding.pry
     if Rails.cache.read(:ar_supercache)
       sub_key = args[1].collect{|a| "#{a.try(:name)} #{a.try(:value)}"}
       Rails.cache.fetch(Digest::SHA1.hexdigest("supercache_#{args[0]}_#{sub_key}")) do
--- spec/lib/supercache/version_spec.rb
+++ spec/lib/supercache/version_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe Supercache do
   it 'has a version number' do
-    expect(Supercache::VERSION).to eq '0.7.2'
+    expect(Supercache::VERSION).to eq '0.7.3'
   end
 end
