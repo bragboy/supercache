dd92cdb5b28c0e23e1e2a5ea249400eb2656a70d
Bragadeesh <bragboy@gmail.com>
2017-12-14 14:31:10 +0530

    Fix for Rails 5.1 and above

--- lib/supercache/super_query_cache.rb
+++ lib/supercache/super_query_cache.rb
@@ -1,5 +1,20 @@
+module SuperQueryCacheRails51AndAbove
+  #Because of https://github.com/rails/rails/commit/84d35da86c14767c737783cb95dd4624632cc1bd
+  def cache_sql(*args, name, &block)
+    if Rails.cache.read(:ar_supercache)
+      sub_key = name.collect{|a| "#{a.try(:name)} #{a.try(:value)}"}
+      Rails.cache.fetch(Digest::SHA1.hexdigest("supercache_#{args[0]}_#{sub_key}")) do
+        super(*args, name, &block)
+      end
+    else
+      super(*args, name, &block)
+    end
+  end
+end
+
 module SuperQueryCache
   def cache_sql(*args, &block)
+    binding.pry
     if Rails.cache.read(:ar_supercache)
       sub_key = args[1].collect{|a| "#{a.try(:name)} #{a.try(:value)}"}
       Rails.cache.fetch(Digest::SHA1.hexdigest("supercache_#{args[0]}_#{sub_key}")) do
@@ -11,4 +26,8 @@ module SuperQueryCache
   end
 end
 
-ActiveRecord::ConnectionAdapters::AbstractAdapter.prepend(SuperQueryCache)
+if (Rails::VERSION::MAJOR > 5) || (Rails::VERSION::MAJOR == 5 && Rails::VERSION::MINOR >= 1)
+  ActiveRecord::ConnectionAdapters::AbstractAdapter.prepend(SuperQueryCacheRails51AndAbove)
+else
+  ActiveRecord::ConnectionAdapters::AbstractAdapter.prepend(SuperQueryCache)
+end
--- spec/lib/supercache/version_spec.rb
+++ spec/lib/supercache/version_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe Supercache do
   it 'has a version number' do
-    expect(Supercache::VERSION).to eq '0.7.1'
+    expect(Supercache::VERSION).to eq '0.7.2'
   end
 end
